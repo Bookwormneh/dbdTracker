aab947eec80517422374842fc4d177af7c14c691
King <bookwormneh@gmail.com>
2020-01-04 09:59:13 -0500

    init

---
+++ dbdTracker/parsers/tagList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbdTracker.parsers
+{
+    class tagList
+    {
+        public static string timesHooked = "";
+        public static string timesHit = "";
+        public static string protectionHits = "";
+
+        public static string timeOnGen = "";
+        public static string timeOnGenCoop = "";
+        public static string skillCheckMissed = "";
+        public static string skillCheckGood = "";
+        public static string skillCheckGreat = "";
+
+        public static string palletsThrown = "";
+        public static string killerStuns = "";
+        public static string killerBlinds = "";
+        public static string flashlightSaves = "";
+        public static string bloodpointsEarned = "";
+
+
+
+        public static string trapperTrapsPlaced = "";
+        public static string trapperSurvivorsTrapped = "";
+        public static string trapperTrapsDisarmed = "";
+
+        public static string wraithTimeCloaked = "";
+
+        public static string hillbillyChainsawRev = "";
+        public static string hillbillyChainsawSprint = "";
+        public static string hillbillyChainsawMiss = "";
+        public static string hillbillyChainsawHit = "";
+
+        public static string nurseBlinkCharge = "";
+        public static string nurseBlinkSprint = "";
+        public static string nurseBlinkCooldown = "";
+        public static string nursePostBlinkHit = "";
+
+        public static string shapeTierOne = "";
+        public static string shapeTierTwo = "";
+        public static string shapeTierThree = "";
+        public static string shapeTierThreeHit = "";
+        public static string shapeStalk = "";
+
+        public static string hagTrapsPlaced = "";
+        public static string hagTeleportToTrap = "";
+        public static string hagTrapsTriggered = "";
+        public static string hagTeleportHits = "";
+
+        public static string doctorTreatmentMode = "";
+        public static string doctorPunishmentMode = "";
+        public static string doctorShockCharge = "";
+        public static string doctorShock = "";
+        public static string doctorShockHit = "";
+        public static string doctorMaddnessOne = "";
+        public static string doctorMaddnessTwo = "";
+        public static string doctorMaddnessThree = "";
+
+        public static string huntressHatchetCharge = "";
+        public static string huntressHatchetThrown = "";
+        public static string huntressHatchetHit = "";
+
+        public static string cannibalChainsawRev = "";
+        public static string cannibalChainsawSprint = "";
+        public static string cannibalChainsawMiss = "";
+        public static string cannibalChainsawHit = "";
+
+        public static string nightmareSnareCharged = "";
+        public static string nightmareSnarePlaced = "";
+        public static string nightmareDreaming = "";
+        public static string nightmareClockWakeUp = "";
+        public static string nightmareGenWakeUp = "";
+        public static string nightmareSurvivorWakeUp = "";
+
+        public static string pigCrouched = "";
+        public static string pigLungeAttack = "";
+        public static string pigLungeHit = "";
+        public static string pigTrapped = "";
+        public static string pigPuzzleBoxSearch = "";
+
+        public static string clownBottlesThrown = "";
+        public static string clownBottlesDirectHit = "";
+        public static string clownSurvivorsIntoxicated = "";
+
+        public static string spiritPhaseCharge = "";
+        public static string spiritPhaseSprint = "";
+        public static string spiritGlitchAppearance = "";
+        public static string spiritPhaseHit = "";
+
+        public static string legionFrenzy = "";
+        public static string legionFrenzyHeartbeat = "";
+        public static string legionFrenzyHit = "";
+
+        public static string plagueVomitCharge = "";
+        public static string plagueVomitSpit = "";
+        public static string plagueVomitInfect = "";
+        public static string plaguePurge = "";
+        public static string plaguePurgeCharge = "";
+        public static string plaguePurgeSpit = "";
+        public static string plaguePurgeInfect = "";
+
+        public static string ghostfaceCrouched = "";
+        public static string ghostfaceStalk = "";
+        public static string ghostfaceStalked = "";
+        public static string ghostfaceExposed = "";
+        public static string ghostfaceRevealed = "";
+
+        public static string demogorgonRev = "";
+        public static string demogorgonLunge = "";
+        public static string demogorgonPlacePortal = "";
+        public static string demogorgonPortalTravel = "";
+        public static string demogorgonLungeHit = "";
+        public static string demogorgonPortalHeartbeat = "";
+        public static string demogorgonPortalClense = "";
+
+        public static string oniCollectBlood = "";
+        public static string oniEnrage = "";
+        public static string oniChargeDemonDash = "";
+        public static string oniDemonDash = "";
+        public static string oniDashHit = "";
+        public static string oniChargeSmash = "";
+        public static string oniSmashHit = "";
+    }
+}
